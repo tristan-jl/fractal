e80510e0bf1885fbcc71b047963f77860891e84f
Tristan Laurens <tristanlaurens@hotmail.com>
2021-06-09 21:05:14 +0100

    Add fractional escape colouring

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"image"
 	"image/color"
 	"image/png"
+	"math"
 	"os"
 )
 
@@ -12,24 +13,26 @@ const (
 	imgWidth               = 2560
 	imgHeight              = 1440
 	samplePerPixel         = 1
-	maxIter                = 100
 	minRe          float64 = -2.5
 	minIm          float64 = -1.2
 	maxIm          float64 = 1.2
+	maxIter                = 25
+	escapeRadius   float64 = 2
 )
 
 const (
-	maxRe    = minRe + (maxIm-minIm)*imgWidth/imgHeight
-	reFactor = (maxRe - minRe) / (imgWidth - 1)
-	imFactor = (maxIm - minIm) / (imgHeight - 1)
+	maxRe         = minRe + (maxIm-minIm)*imgWidth/imgHeight
+	rePixelSize   = (maxRe - minRe) / (imgWidth - 1)
+	imPixelSize   = (maxIm - minIm) / (imgHeight - 1)
+	escapeRadius2 = escapeRadius * escapeRadius
 )
 
-func escapeTime(cRe, cIm float64) int {
-	var x, y, x2, y2 float64
+func escapeValue(cRe, cIm float64) (float64, int) {
+	x, y, x2, y2 := 0., 0., 0., 0.
 
 	for i := 0; i < maxIter; i++ {
-		if x2+y2 > 4 {
-			return i
+		if x2+y2 > escapeRadius2 {
+			return x2 + y2, i
 		}
 		y = 2*x*y + cIm
 		x = x2 - y2 + cRe
@@ -37,7 +40,16 @@ func escapeTime(cRe, cIm float64) int {
 		y2 = y * y
 	}
 
-	return maxIter
+	return x2 + y2, maxIter
+}
+
+func fractionalEscapeValue(cRe, cIm float64) float64 {
+	z, n := escapeValue(cRe, cIm)
+
+	logBase := 1. / math.Log(2.)
+	logHalfBase := math.Log(0.5) * logBase
+
+	return 5. + float64(n) - logHalfBase - math.Log(math.Log(z))*logBase
 }
 
 func main() {
@@ -45,19 +57,18 @@ func main() {
 	img := image.NewNRGBA(image.Rect(0, 0, imgWidth, imgHeight))
 
 	for j := 0; j < imgHeight; j++ {
-		cIm := maxIm - float64(j)*imFactor
+		cIm := maxIm - float64(j)*imPixelSize
 		for i := 0; i < imgWidth; i++ {
-			cRe := minRe + float64(i)*reFactor
+			cRe := minRe + float64(i)*rePixelSize
 
-			iter := escapeTime(cRe, cIm)
+			value := fractionalEscapeValue(cRe, cIm)
 
-			if iter >= maxIter {
-				img.SetNRGBA(i, j, color.NRGBA{0, 0, 0, 255})
-			} else {
-				r, g, b := hslToRgb(float64(iter)/float64(maxIter), 1, 0.5)
+			if value < maxIter {
+				r, g, b := hslToRgb(value/float64(maxIter), 1, 0.5)
 				img.SetNRGBA(i, j, color.NRGBA{uint8(r * 255), uint8(g * 255), uint8(b * 255), 255})
+			} else {
+				img.SetNRGBA(i, j, color.NRGBA{0, 0, 0, 255})
 			}
-
 		}
 	}
 
